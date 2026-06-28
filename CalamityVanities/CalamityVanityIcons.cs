2ff56015db1cecdeed55cfefba80c9a4ffabed12
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2026-06-28 00:38:47 -0400

    calamity vanities

---
+++ CalamityVanities/CalamityVanityIcons.cs
@@ -0,0 +1,24 @@
+﻿using CalValEX.Biomes;
+using EnvironmentIcons.API;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EnvironmentIconsCrossmod.CalamityVanities;
+
+public static class CalamityVanitiesInfo
+{
+	internal const string ModName = "CalValEX";
+	public static Mod Instance = null;
+	public static bool Enabled { get; } = ModLoader.TryGetMod(ModName, out Instance);
+}
+
+[JITWhenModsEnabled(CalamityVanitiesInfo.ModName)]
+public class AstralBlight_EnvironmentIcon : ModBiomeEnvironmentIcon
+{
+	protected override ModBiome Biome => ModContent.GetInstance<AstralBlight>();
+
+	public override Condition Applies() => new Condition("",
+		IsBiomeActive);
+
+	public override bool IsLoadingEnabled(Mod mod) => CalamityVanitiesInfo.Enabled;
+}
