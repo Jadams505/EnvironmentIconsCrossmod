bac9c6a58997d3c1ea970f18750196561e2d4273
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2026-08-06 20:16:23 -0400

    depths support

---
+++ TheDepths/TheDepthsIcons.cs
@@ -0,0 +1,39 @@
+﻿using EnvironmentIcons.API;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EnvironmentIconsCrossmod.TheDepths;
+
+public static class TheDepthsIcons
+{
+	internal const string ModName = "TheDepths";
+	public static Mod Instance = null;
+	public static bool Enabled { get; } = ModLoader.TryGetMod(ModName, out Instance);
+
+	public const string ConditionPath = $"Mods.EnvironmentIconsCrossmod.Conditions.{ModName}";
+	public const string DisplayNamePath = $"Mods.EnvironmentIconsCrossmod.DisplayNames.{ModName}";
+}
+
+[JITWhenModsEnabled(TheDepthsIcons.ModName)]
+public class DepthsBiome_EnvironmentIcon : ModBiomeEnvironmentIcon
+{
+	public const string BiomeName = "DepthsBiome";
+	protected override ModBiome Biome => ModContent.Find<ModBiome>(TheDepthsIcons.ModName, BiomeName);
+
+	public override Condition Applies() => new Condition($"{TheDepthsIcons.ConditionPath}.{BiomeName}",
+		IsBiomeActive);
+
+	public override bool IsLoadingEnabled(Mod mod)
+	{
+		if (!TheDepthsIcons.Enabled)
+			return false;
+
+		if (!ModContent.TryFind<ModBiome>(TheDepthsIcons.ModName, BiomeName, out _))
+		{
+			EnvironmentIconsCrossmod.LogMissingBiome(TheDepthsIcons.ModName, BiomeName, GetType().Name);
+			return false;
+		}
+
+		return true;
+	}
+}
