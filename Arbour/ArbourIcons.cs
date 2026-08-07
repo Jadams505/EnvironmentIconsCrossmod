5edee2445b657e021a2752d4792658f9926cfdba
Jacob Adams <56134537+Jadams505@users.noreply.github.com>
2026-08-07 18:25:46 -0400

    arbour

---
+++ Arbour/ArbourIcons.cs
@@ -0,0 +1,40 @@
+﻿using EnvironmentIcons.API;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EnvironmentIconsCrossmod.Arbour;
+
+public static class ArbourIcons
+{
+	internal const string ModName = "Arbour";
+	public static Mod Instance = null;
+	public static bool Enabled { get; } = ModLoader.TryGetMod(ModName, out Instance);
+
+	public const string ConditionPath = $"Mods.EnvironmentIconsCrossmod.Conditions.{ModName}";
+	public const string DisplayNamePath = $"Mods.EnvironmentIconsCrossmod.DisplayNames.{ModName}";
+}
+
+[JITWhenModsEnabled(ArbourIcons.ModName)]
+public class ArborBiome_EnvironmentIcon : ModBiomeEnvironmentIcon
+{
+	public const string BiomeName = "ArborBiome";
+	protected override ModBiome Biome => ModContent.Find<ModBiome>(ArbourIcons.ModName, BiomeName);
+
+	public override Condition Applies() => new Condition($"{ArbourIcons.ConditionPath}.{BiomeName}",
+		IsBiomeActive);
+
+	public override bool IsLoadingEnabled(Mod mod)
+	{
+		if (!ArbourIcons.Enabled)
+			return false;
+
+		if (!ModContent.TryFind<ModBiome>(ArbourIcons.ModName, BiomeName, out _))
+		{
+			EnvironmentIconsCrossmod.LogMissingBiome(ArbourIcons.ModName, BiomeName, GetType().Name);
+			return false;
+		}
+
+		return true;
+	}
+}
+
